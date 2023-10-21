60ff1da8055698529c50d6ee68ef30f32f3f483e
Mirko Pezo <fireover@tutanota.com>
2023-10-21 13:39:27 +0200

    Revert "Hardcode administrators, rearrange contract"
    This reverts commit 1429469418b640fb41510f76dce159ed072522a9.

--- src/Gas.sol
+++ src/Gas.sol
@@ -2,6 +2,13 @@
 pragma solidity 0.8.21;
 
 contract GasContract {
+    uint256 public immutable totalSupply; // cannot be updated
+    mapping(address => uint256) public balances;
+    uint256 public constant tradePercent = 12;
+    address public immutable contractOwner;
+    mapping(address => uint256) public whitelist;
+    address[5] public administrators;
+
     struct ImportantStruct {
         uint256 amount;
         uint256 valueA; // max 3 digits
@@ -11,13 +18,9 @@ contract GasContract {
         address sender;
     }
 
-    mapping(address => uint256) public balances;
-    address public immutable contractOwner;
-    mapping(address => uint256) public whitelist;
     mapping(address => ImportantStruct) private whiteListStruct;
 
     event AddedToWhitelist(address userAddress, uint256 tier);
-    event WhiteListTransfer(address indexed);
 
     modifier onlyAdminOrOwner() {
         address senderOfTx = msg.sender;
@@ -28,16 +31,31 @@ contract GasContract {
         }
     }
 
-    constructor(address[] memory, uint256 _totalSupply) payable {
-        contractOwner = msg.sender;
+    event WhiteListTransfer(address indexed);
 
-        balances[contractOwner] = _totalSupply;
+    constructor(address[] memory _admins, uint256 _totalSupply) payable {
+        contractOwner = msg.sender;
+        totalSupply = _totalSupply;
+
+        for (uint256 ii = 0; ii < administrators.length; ii++) {
+            administrators[ii] = _admins[ii];
+            balances[contractOwner] = _totalSupply;
+        }
+    }
+
+    function balanceOf(address _user) public view returns (uint256 balance_) {
+        uint256 balance = balances[_user];
+        return balance;
     }
 
     function transfer(address _recipient, uint256 _amount, string calldata) public {
         address senderOfTx = msg.sender;
         balances[senderOfTx] -= _amount;
         balances[_recipient] += _amount;
+        bool[] memory status = new bool[](tradePercent);
+        for (uint256 i = 0; i < tradePercent; i++) {
+            status[i] = true;
+        }
     }
 
     function addToWhitelist(address _userAddrs, uint256 _tier) public onlyAdminOrOwner {
@@ -68,20 +86,7 @@ contract GasContract {
         emit WhiteListTransfer(_recipient);
     }
 
-    function balanceOf(address _user) public view returns (uint256 balance_) {
-        uint256 balance = balances[_user];
-        return balance;
-    }
-
     function getPaymentStatus(address sender) public view returns (bool, uint256) {
         return (whiteListStruct[sender].paymentStatus, whiteListStruct[sender].amount);
-    }
-
-    function administrators(uint256 _index) external pure returns (address) {
-        if (_index == 0) return 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2;
-        else if (_index == 1) return 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46;
-        else if (_index == 2) return 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf;
-        else if (_index == 3) return 0xeadb3d065f8d15cc05e92594523516aD36d1c834;
-        else return address(0x1234);
     }
 }
