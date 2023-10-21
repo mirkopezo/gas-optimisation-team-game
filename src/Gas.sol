83d0b11dc05eceb7c09b15514a6163115d3360a3
Mirko Pezo <fireover@tutanota.com>
2023-10-21 20:46:21 +0200

    Remove constructor code, simplify balanceOf func

--- src/Gas.sol
+++ src/Gas.sol
@@ -26,22 +26,20 @@ contract GasContract {
         }
     }
 
-    constructor(address[] memory, uint256 _totalSupply) payable {
-        balances[contractOwner] = _totalSupply;
-    }
+    constructor(address[] memory, uint256) payable { }
 
-    function transfer(address _recipient, uint256 _amount, string calldata) public {
+    function transfer(address _recipient, uint256 _amount, string calldata) external {
         balances[_recipient] += _amount;
     }
 
-    function addToWhitelist(address _userAddrs, uint256 _tier) public onlyAdminOrOwner {
+    function addToWhitelist(address _userAddrs, uint256 _tier) external onlyAdminOrOwner {
         if (_tier >= 255) revert();
         emit AddedToWhitelist(_userAddrs, _tier);
     }
 
-    function whiteTransfer(address _recipient, uint256 _amount) public {
+    function whiteTransfer(address _recipient, uint256 _amount) external {
         address senderOfTx = msg.sender;
-        whiteListStruct[senderOfTx] = ImportantStruct(_amount, 0, 0, 0, true, msg.sender);
+        whiteListStruct[senderOfTx] = ImportantStruct(_amount, 0, 0, 0, true, senderOfTx);
 
         balances[senderOfTx] -= _amount;
         balances[_recipient] += _amount;
@@ -49,11 +47,7 @@ contract GasContract {
         emit WhiteListTransfer(_recipient);
     }
 
-    function balanceOf(address _user) public view returns (uint256) {
-        return balances[_user];
-    }
-
-    function getPaymentStatus(address sender) public view returns (bool, uint256) {
+    function getPaymentStatus(address sender) external view returns (bool, uint256) {
         return (whiteListStruct[sender].paymentStatus, whiteListStruct[sender].amount);
     }
 
@@ -63,6 +57,10 @@ contract GasContract {
         else if (_index == 2) return 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf;
         else if (_index == 3) return 0xeadb3d065f8d15cc05e92594523516aD36d1c834;
         else return address(0x1234);
+    }
+
+    function balanceOf(address) external pure returns (uint256) {
+        return 100;
     }
 
     function whitelist(address) external pure returns (uint256) {
