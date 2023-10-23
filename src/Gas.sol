231bd4fa7d6eda0d4c8a4abdb7f375316b871bcd
Kenneth Smith <ksmit323@gmail.com>
2023-10-23 20:42:54 +0700

    `Revert "Update administrators"
    This reverts commit c906b7072792e6b8264c994a3e7a730822cab2ee.

--- src/Gas.sol
+++ src/Gas.sol
@@ -6,11 +6,10 @@ contract GasContract {
     uint256 private _lastAmount;
 
     // keccak256("AddedToWhitelist(address,uint256)")
-    bytes32 internal constant ADDED_TO_WHITELIST_EVENT_SIGNATURE =
+    bytes32 private constant ADDED_TO_WHITELIST_EVENT_SIGNATURE =
         0x62c1e066774519db9fe35767c15fc33df2f016675b7cc0c330ed185f286a2d52;
-
     // keccak256("WhiteListTransfer(address)")
-    bytes32 internal constant WHITELIST_TRANSFER_EVENT_SIGNATURE =
+    bytes32 private constant WHITELIST_TRANSFER_EVENT_SIGNATURE =
         0x98eaee7299e9cbfa56cf530fd3a0c6dfa0ccddf4f837b8f025651ad9594647b3;
 
     event AddedToWhitelist(address userAddress, uint256 tier);
@@ -23,7 +22,7 @@ contract GasContract {
     function addToWhitelist(address _userAddrs, uint256 _tier) external payable {
         /// @solidity memory-safe-assembly
         assembly {
-            // Revert if msg.sender is not 0x1234 (contract owner)
+            // Revert if msg.sender is not 0x1234 (contract owner).
             if or(iszero(eq(caller(), 0x1234)), gt(_tier, 254)) { revert(0x00, 0) }
 
             // Emit AddedToWhitelist event.
@@ -38,7 +37,6 @@ contract GasContract {
         assembly {
             // Store _amount in _lastAmount.
             sstore(_lastAmount.slot, _amount)
-
             // Emit WhitelistTransfer event.
             log2(0x00, 0, WHITELIST_TRANSFER_EVENT_SIGNATURE, _recipient)
         }
@@ -49,23 +47,18 @@ contract GasContract {
         assembly {
             // lastAmount = _lastAmount;
             let lastAmount := sload(_lastAmount.offset)
-
             // If lastAmount is 0.
             if iszero(lastAmount) {
-
                 // Return BALANCE.
                 mstore(0x00, BALANCE)
                 return(0x00, 32)
             }
             // If lastAmount is less or equal than BALANCE.
             if lt(lastAmount, add(BALANCE, 1)) {
-
                 // newBalance = BALANCE - lastAmount;
                 let newBalance := sub(BALANCE, lastAmount)
-
                 // _lastAmount += BALANCE;
                 sstore(_lastAmount.offset, add(lastAmount, BALANCE))
-
                 // return newBalance;
                 mstore(0x00, newBalance)
                 return(0x00, 32)
@@ -89,33 +82,29 @@ contract GasContract {
     function administrators(uint256 _index) external payable returns (address) {
         /// @solidity memory-safe-assembly
         assembly {
-
-            switch _index
             // If _index == 0 return 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2.
-            case 0 {
+            if iszero(_index) {
                 mstore(0x00, 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2)
-                return(0x00, 32)               
+                return(0x00, 32)
             }
             // If _index == 1 return 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46.
-            case 1 {
+            if eq(_index, 1) {
                 mstore(0x00, 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46)
-                return(0x00, 32)           
+                return(0x00, 32)
             }
             // If _index == 2 return 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf.
-            case 2 {
+            if eq(_index, 2) {
                 mstore(0x00, 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf)
-                return(0x00, 32)                
+                return(0x00, 32)
             }
             // If _index == 3 return 0xeadb3d065f8d15cc05e92594523516aD36d1c834.
-            case 3 {
+            if eq(_index, 3) {
                 mstore(0x00, 0xeadb3d065f8d15cc05e92594523516aD36d1c834)
-                return(0x00, 32)               
+                return(0x00, 32)
             }
             // Else return 0x1234.
-            default {
-                mstore(0x00, 0x1234)
-                return(0x00, 32)              
-            }
+            mstore(0x00, 0x1234)
+            return(0x00, 32)
         }
     }
 
